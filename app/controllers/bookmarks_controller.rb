4954553ab86cfa3d7568239f613dc1f2506c1910
FabienLH <133055509+CartonSlip@users.noreply.github.com>
2024-08-09 10:59:43 +0200

    pas commit hier

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmarks_path(@bookmarks)
+    else
+      render :new, status: :unprocessable_entity
+    end
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+
+  end
+end
